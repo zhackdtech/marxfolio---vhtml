39749c95c4fb9e2d693523c3177f44a533905202
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2024-03-12 23:22:48 +0800

    fixed animation problems
    -fixed where you need to click on the burger 2 times before it shows the menu links
    -fixed where you need to click on the navlinks 2 times before the animSlider triggers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,43 @@
+var navCounter = 0;
+
 $("#burgerMenuBtn").on("click", function () {
   $(".bar1").toggleClass("change");
   $(".bar2").toggleClass("change");
   $(".bar3").toggleClass("change");
-//   slideInLeftAnim();
+
+  if(navCounter === 0)
+  {
+    navSlideDown();
+  }
+  else
+  {
+    navSlideUp();
+  }
+  console.log(navCounter);
+  // slideInLeftAnim();
 });
-function slideInLeftAnim()
+$(".navlink").on("click", function () {
+  slideInLeftAnim();
+  console.log("nav clicked");
+});
+function navSlideDown()
 {
-    $(".animSlider").toggleClass("slideInLeft");
+  $(".navOverlay").toggleClass("fadeInDown");
+  navCounter = 1;
+}
+function navSlideUp()
+{
+  $(".navOverlay").toggleClass("fadeOutUp");
+  navCounter = 0;
+  setTimeout(() => {  removeUnnecessaryClass(); }, 100);
+}
+function removeUnnecessaryClass()
+{
+  $(".navOverlay").removeClass("fadeOutUp");
+  $(".navOverlay").removeClass("fadeInDown");
+}
+function slideInLeftAnim() {
+  // play the animation then remove its class when it's done
+  $(".animSlider").toggleClass("slideInLeft");
+setTimeout(() => {  $(".animSlider").removeClass("slideInLeft"); }, 700);
 }
